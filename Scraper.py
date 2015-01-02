f60f5340d032203ed8c219233f47bff17c1d779d
sunnyddelight <sunnyddelight@gmail.com>
2015-01-02 00:03:06 -0600

    being awesome

--- Scraper.py
+++ Scraper.py
@@ -14,6 +14,8 @@ br.addheaders = [('User-agent', 'Chrome')]
 #br.open('https://github.com/login')
 #for f in br.forms():
 #    print f
+subj='CS'
+
 
 br.open('https://eas.admin.uillinois.edu/eas/servlet/EasLogin?redirect=https://webprod.admin.uillinois.edu/ssa/servlet/SelfServiceLogin?appName=edu.uillinois.aits.SelfServiceLogin&dad=BANPROD1')
 br.select_form(nr=0)
@@ -21,17 +23,23 @@ br.form['inputEnterpriseId']='syduan2'
 br.form['password']='Rwylet314?'
 br.submit()
 
-print br.open('https://ui2web1.apps.uillinois.edu/BANPROD1/bwskfcls.p_sel_crse_search').read()
-for f in br.forms():
-    print f
+br.open('https://ui2web1.apps.uillinois.edu/BANPROD1/bwskfcls.p_sel_crse_search')
+
 br.select_form(nr=1)
-
 br.form['p_term']=['120151']
-br.submit()
+br.open(br.form.click())
+#br.open('https://ui2web1.apps.uillinois.edu/BANPROD1/bwckgens.p_proc_term_date')
+forms=br.forms()
+forms.next()
+focus=forms.next()
+focus.set_value([subj],  nr=13)
 
-br.open('https://ui2web1.apps.uillinois.edu/BANPROD1/bwckgens.p_proc_term_date')
+    #.set_value(subj,  nr=0)
 
+#br.form['sel_subj']=[subj]
+print br.open(focus.click(name='SUB_BTN')).read()
 for f in br.forms():
     print f
+
 
 
