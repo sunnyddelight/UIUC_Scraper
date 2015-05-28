082c17288816b28cbda521c7f55c871c23725673
Sunny Duan <sunnyd@google.com>
2015-05-28 12:08:29 -0400

    added usage clause

--- get_pdfs.sh
+++ get_pdfs.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ $# -ne 2 ]
+then
+  echo -e "\nUsage:\n $0 URL zip_filename\n"
+  exit 0
+fi
 temp_dir=$(mktemp -d)
 wget -r -l 1 -nd -nH -A pdf -P $temp_dir $1
 zip $2 -j $temp_dir/*.pdf
